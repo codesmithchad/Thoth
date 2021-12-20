17aa449f01b103b265ef97663454c0717a819f16
Hyuck Hur <ajiaco@stunitas.com>
2021-12-20 16:45:55 +0900

    add cgsize extension

---
+++ Sources/Thoth/Thoth+CGSize.swift
@@ -0,0 +1,23 @@
+//
+//  Thoth+CGSize.swift
+//  Thoth
+//
+//  Created by Ajiaco on 2021/12/20.
+//
+
+import UIKit
+
+extension CGSize {
+    var ratio: CGFloat {
+        if width == 0 { return 0.0 }
+        return height / width
+    }
+    var ratioReverse: CGFloat {
+        if height == 0 { return 0.0 }
+        return width / height
+    }
+
+    var scaled: Self {
+        return Self.init(width: width * UIScreen.main.scale, height: height * UIScreen.main.scale)
+    }
+}
