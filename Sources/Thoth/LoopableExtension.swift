fcde2476c2031e788667b3b71d0a9ada3608e846
Hyuck Hur <ajiaco@stunitas.com>
2022-02-16 13:37:59 +0900

    add PersistenceManager

---
+++ Sources/Thoth/LoopableExtension.swift
@@ -0,0 +1,35 @@
+//
+//  LoopableExtension.swift
+//  Thoth
+//
+//  Created by Ajiaco on 2022/02/16.
+//
+
+import Foundation
+
+protocol Loopable {
+    func allProperties() throws -> [String: Any]
+}
+
+extension Loopable {
+    func allProperties() throws -> [String: Any] {
+        var result: [String: Any] = [:]
+        let mirror = Mirror(reflecting: self)
+
+        // Optional check to make sure we're iterating over a struct or class
+        guard let style = mirror.displayStyle, style == .struct || style == .class else {
+            throw NSError()
+        }
+
+        for (property, value) in mirror.children {
+            guard let property = property else {
+                continue
+            }
+
+            result[property] = value
+        }
+
+        return result
+    }
+}
+
